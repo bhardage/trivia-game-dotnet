5dc32895939250b5a997a84a3ad7b06ca787d9ee
bhardage <bhardage@gmail.com>
2018-11-02 10:05:34 -0500

    Add SlackUtils to extract username

---
+++ TriviaGame/Utils/SlackUtils.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace TriviaGame.Utils
+{
+    public class SlackUtils
+    {
+        private const string SLACK_ID_PATTERN = "^<@(.+?)(\\|.*)?>$";
+
+        public static string NormalizeId(string slackId)
+        {
+            if (slackId == null)
+            {
+                return null;
+            }
+
+            string extractedId = slackId;
+            Match match = Regex.Matches(extractedId, SLACK_ID_PATTERN).FirstOrDefault();
+
+            if (match != null)
+            {
+                extractedId = match.Groups[1].Value;
+            }
+
+            return extractedId;
+        }
+    }
+}
---
+++ TriviaGameTests/Utils/SlackUtilsTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TriviaGame.Utils;
+
+namespace TriviaGameTests.Utils
+{
+    [TestClass]
+    public class SlackUtilsTest
+    {
+        [TestMethod]
+        public void TestNormalizeIdWithNullSlackId()
+        {
+            Assert.IsNull(SlackUtils.NormalizeId(null));
+        }
+
+        [TestMethod]
+        public void TestNormalizeIdWithInvalidSlackId()
+        {
+            Assert.AreEqual("garbage", SlackUtils.NormalizeId("garbage"));
+        }
+
+        [TestMethod]
+        public void TestNormalizeIdWithValidSlackId()
+        {
+            Assert.AreEqual("U12345", SlackUtils.NormalizeId("<@U12345>"));
+        }
+
+        [TestMethod]
+        public void TestNormalizeIdWithValidSlackIdAndUsername()
+        {
+            Assert.AreEqual("U12345", SlackUtils.NormalizeId("<@U12345|jsmith>"));
+        }
+
+        [TestMethod]
+        public void TestNormalizeIdWithValidSlackIdAndEmptyUsername()
+        {
+            Assert.AreEqual("U12345", SlackUtils.NormalizeId("<@U12345|>"));
+        }
+    }
+}
